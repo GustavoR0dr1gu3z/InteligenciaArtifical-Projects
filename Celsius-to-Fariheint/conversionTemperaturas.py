b2f263cd37dce631a941dfadb8d3eb00922ba2f1
Gustavo Calzada <tym.latam1@gmail.com>
2024-05-15 21:55:13 -0600

    ejercicio 1

---
+++ Celsius-to-Fariheint/conversionTemperaturas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 16:45:02 2024
+
+@author: desarrolloweb
+"""
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sbn
+import matplotlib.pyplot as plt
